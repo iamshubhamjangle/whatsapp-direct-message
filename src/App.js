6bc6ae45a5d7286f9205b81610d4e38bd2b9b8ec
Shubham Jangle <jangleshubham13@gmail.com>
2022-11-29 15:13:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CountryCodeDropdown from "./CountryCodeDropdown";
+import Navbar from "./Navbar";
 
 function App() {
+  const [prefix, setPrefix] = useState("91");
+  const [number, setNumber] = useState("9673735362");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <main className="container mt-4 text-center d-flex flex-column justify-content-center">
+        <h2 className="">Send a message without adding to contacts</h2>
+        <div className="row mt-4">
+          <div className="col-md-3 my-1 my-md-0">
+            <CountryCodeDropdown setPrefix={setPrefix} />
+          </div>
+          <div className="col-md-9  my-1 my-md-0">
+            <input
+              className="form-control"
+              type="text"
+              placeholder="1234567890"
+              onChange={(e) => setNumber(e.target.value)}
+              value={number}
+            ></input>
+          </div>
+        </div>
+        <button className="btn btn-success mt-4 w-100">
+          <a
+            href={`https://wa.me/${prefix}${number}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Send
+          </a>
+        </button>
+      </main>
     </div>
   );
 }
